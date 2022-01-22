1e18d8000abd482e5e4c863b5a647bb7b6cd45ae
Quan Dao <quan.dao912@gmail.com>
2022-01-22 21:46:59 +0200

    update app

--- index.js
+++ index.js
@@ -82,10 +82,8 @@ const errorHandler = (error, request, response, next) => {
   if (error.name === 'CastError') {
     return response.status(400).send({ error: 'malformatted id' })
   }
-
   next(error)
 }
-
 
 // handler of requests with result to errors
 app.use(errorHandler);
