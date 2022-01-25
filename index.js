489eb76b18196d6e7eda91446d7bbc3e24d25b70
Quan Dao <quan.dao912@gmail.com>
2022-01-25 09:20:17 +0200

    uibuild

--- index.js
+++ index.js
@@ -84,7 +84,7 @@ const unknownEndpoint = (request, response) => {
 app.use(unknownEndpoint);
 
 const errorHandler = (error, request, response, next) => {
-
+  console.log('handle err be', error);
   if (error.name === 'CastError') {
     return response.status(400).send({ error: 'malformatted id' })
   } else if (error.name === 'ValidationError') {
